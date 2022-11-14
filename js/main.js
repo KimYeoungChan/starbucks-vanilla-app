7ec69b0f18b925105275cbfb60075cce9b59f94a
= <=>
2022-11-14 16:04:20 +0900

    [Feat] :ab: promotion slider section

--- js/main.js
+++ js/main.js
@@ -81,3 +81,20 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
+new Swiper('.promotion .swiper-container', {
+  slidesPerView:3, // 한번에 보여줄 슬라이드 개수
+  spaceBetwenn: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop:true,
+  // autoplay: {
+  //   delay: 500
+  // }
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clicktable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    preEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+}); 
