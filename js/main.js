7111012bc981261fcd8a3cdf356edcd724cd291e
= <=>
2022-11-16 11:34:27 +0900

    [Feat] :ab: youtube_api_section

--- js/main.js
+++ js/main.js
@@ -76,25 +76,64 @@ fadeEls.forEach(function (fadeEl, index) {
 /**
  * 슬라이드 요소 관리
  */
-new Swiper('.notice-line .swiper-container', {
+ new Swiper('.notice-line .swiper-container', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
 new Swiper('.promotion .swiper-container', {
-  slidesPerView:3, // 한번에 보여줄 슬라이드 개수
-  spaceBetwenn: 10, // 슬라이드 사이 여백
-  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
-  loop:true,
-  // autoplay: {
-  //   delay: 500
-  // }
-  pagination: {
-    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
-    clicktable: true // 사용자의 페이지 번호 요소 제어
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
   },
-  navigation: {
-    preEl: '.promotion .swiper-prev',
-    nextEl: '.promotion .swiper-next'
+  loop: true, // 반복 재생 여부
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
   }
-}); 
+})
+
+const procmtionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click',function() {
+  isHidePromotion = !isHidePromotion //isHidePromotion는 false값의 반대값임 true;
+  if (isHidePromotion) {
+    // 숨김 처리!
+    procmtionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    procmtionEl.classList.remove('hide');
+  }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    { // 옵션
+    y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+    repeat:-1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+    yoyo:true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+    ease: Power1.easeInOut, // Easing 함수 적용.
+    delay: random(0, delay) //  얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
