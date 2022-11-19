4158e723d4828b08541996905a1a1289b387ce53
= <=>
2022-11-19 19:29:55 +0900

    [Feat] :ab: swiper_setion

--- js/main.js
+++ js/main.js
@@ -98,7 +98,18 @@ new Swiper('.promotion .swiper-container', {
     prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
     nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
   }
-})
+});
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop:true,
+  spaceBetween:30,
+  slidesPerView:5,
+  navigation: {
+    prevEl:'.awards .swiper-prev',
+    nextEl:'.awards .swiper-next'
+  }
+});
+
 
 const procmtionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -142,13 +153,14 @@ floatingObject('.floating3', 1.5, 20);
  * 요소가 화면에 보여짐 여부에 따른 요소 관리
  */
 // 관리할 요소들 검색!
-const spyEls = document.querySelectorAll('.section.scroll-spy');
+const spyEls = document.querySelectorAll('section.scroll-spy')
 // 요소들 반복 처리!
-spyEls.forEach(function(spyEl){
-  new ScrollMagic.Scene({  // 감시할 장면(Scene)을 추가
-    triggerElement : spyEl, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
-  })
-  .setClassToggle()
-  .addTo();
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
